1335aa9711252029895a829ad2d08af0b9f17fda
johnbyeon <ghks0115@gmail.com>
2025-10-30 11:17:32 +0900

    popup log 수정

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,22 +6,20 @@ import AppRoutes from './routes/AppRoutes'
 
 
 export default function App() {
+  
   console.log('🔥 VITE_SERVER_ORIGIN:', import.meta.env.VITE_SERVER_ORIGIN)
   useEffect(() => {
     initAuthTimers()
-
-    const listener = (event) => {
-    if (!event.data || !event.data.access_token) return
-    console.log('[Global OAuth Message Received]', event.origin, event.data)
-
-    const { loginFromResponse } = useAuthStore.getState()
-    loginFromResponse(event.data)
-  }
-
-  window.addEventListener('message', listener)
-  return () => window.removeEventListener('message', listener)
   }, [])
-
+    useEffect(() => {
+    const listener = (event) => {
+      console.log('[Global OAuth Message Received]', event.origin, event.data)
+      if (!event.data?.access_token) return
+      loginFromResponse(event.data)
+    }
+    window.addEventListener('message', listener)
+    return () => window.removeEventListener('message', listener)
+  }, [loginFromResponse])
   return (
     <>
       {/* 전역 OAuth 메시지 브리지: 팝업에서 postMessage 수신 */}
